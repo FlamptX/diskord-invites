826ead299f140051614722733c33141746703af3
FlamptX <dino.shark2006@gmail.com>
2021-10-28 15:56:18 +0200

    Remove Client due to bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="diskord-invites",
-    version="0.1.0",
+    version="0.1.1",
     author="Flampt",
     license="MIT",
     description="Python library for simple invites tracking.",
