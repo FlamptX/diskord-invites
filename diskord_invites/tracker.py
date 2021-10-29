143179ccc0813cc66ed47d6e927f3e7358c5af99
FlamptX <dino.shark2006@gmail.com>
2021-10-29 14:05:05 +0200

    v0.2 - Added option to fetch all invites when the bot is booted to make the member_join decorator always work, added docstring and an option to suppress the logs from the library

--- diskord_invites/tracker.py
+++ diskord_invites/tracker.py
@@ -2,12 +2,32 @@ from diskord import Member, TextChannel, VoiceChannel
 from diskord.ext.commands import Bot, Cog
 from typing import Union, Callable, Awaitable
 from asyncio import sleep, get_event_loop, iscoroutinefunction
+import time
 
 class Tracker(Cog):
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: Bot, fetch_invites: bool = False, suppress_logs: bool = False):
+        bot.add_cog(self)
+
         self.bot = bot
         self.funcs = []
         self.invites = []
+        self.suppress_logs = suppress_logs
+
+        if fetch_invites:
+            self.bot.add_listener(self.on_ready)
+
+    async def _fetch_invites(self):
+        t = time.time()
+        for g in self.bot.guilds:
+            invites = await g.invites()
+            for i in invites:
+                self.invites.append(i)
+            await sleep(0.5)
+        if not self.suppress_logs:
+            print(f"All invites were fetched ({round(time.time() - t)}s).")
+
+    async def on_ready(self):
+        await self._fetch_invites()
 
     @Cog.listener()
     async def on_invite_create(self, invite):
@@ -31,6 +51,12 @@ class Tracker(Cog):
                         loop.create_task(z(member, x.inviter, x.channel))
 
     def member_join(self, coro: Callable[[Member, Member, Union[TextChannel, VoiceChannel]], Awaitable[None]]):
+        """
+        A decorator that listens for a member_join event
+
+        :param coro: The coroutine passed for the decorator
+        :raises TypeError: When the passed function is not a coroutine
+        """
         if not iscoroutinefunction(coro):
             raise TypeError(f"{coro} is not a coroutine")
         self.funcs.append(coro)
